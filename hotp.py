a734f016d0b3c666df6c3efc8ad82208260432e3
Issam Abkadri <iabkadri@student.1337.ma>
2024-11-09 10:47:44 +0100

    add counter as a paramter

--- hotp.py
+++ hotp.py
@@ -7,14 +7,12 @@ class HOTP:
     An implementation of the HMAC-Based One-Time Password (HOTP) algorithm.
     """
 
-    counter = 0
-
     @classmethod
-    def generate(cls, key: bytes) -> int:
+    def generate(cls, key: bytes, counter: int) -> int:
         """
         Return the number whose binary representation is s.
         """
-        hmac_result = hmac.new(key, bytes(HOTP.counter), digestmod=sha1).digest()
+        hmac_result = hmac.new(key, bytes(counter), digestmod=sha1).digest()
         offset = hmac_result[-1] & 0xf
         bin_code = ((hmac_result[offset] & 0x7f) << 24
                     | (hmac_result[offset + 1] & 0xff) << 16

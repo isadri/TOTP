df84daff9d0005e06051be75e1c982e4bbe84c9e
Issam Abkadri <iabkadri@student.1337.ma>
2024-11-09 14:38:03 +0100

    make TOTP class constructable

--- totp.py
+++ totp.py
@@ -8,13 +8,13 @@ class TOTP:
     An implementation of the Time-based One-Time Password (TOTP) algorithm.
     """
 
-    TIME_STEP = 30
-    initial_time = time.time()
+    def __init__(self) -> None:
+        self.TIME_STEP = 30
+        self.initial_time = time.time()
 
-    @classmethod
-    def generate(cls, key: bytes) -> int:
+    def generate(self, key: bytes) -> int:
         """
         Generate a TOTP value.
         """
-        nbr_time_steps = floor((time.time() - cls.initial_time) / cls.TIME_STEP)
+        nbr_time_steps = floor((time.time() - self.initial_time) / self.TIME_STEP)
         return HOTP.generate(key, str(nbr_time_steps).encode())

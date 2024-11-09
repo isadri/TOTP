5ffb4d904daa670ae8dddc4f7792d894a2fbdcc9
Issam Abkadri <iabkadri@student.1337.ma>
2024-11-09 18:02:30 +0100

    calculate the time value

--- totp.py
+++ totp.py
@@ -1,3 +1,5 @@
+import calendar
+from datetime import datetime
 from math import floor
 import time
 from hotp import HOTP
@@ -10,11 +12,10 @@ class TOTP:
 
     def __init__(self) -> None:
         self.time_steps: int = 30
-        self.initial_time: float = 0.0
 
     def generate(self, key: bytes) -> int:
         """
         Generate a TOTP value.
         """
-        nbr_time_steps = floor((time.time() - self.initial_time) / self.time_steps)
-        return HOTP.generate(key, nbr_time_steps)
+        nbr = floor(calendar.timegm(datetime.now().utctimetuple()) / self.time_steps)
+        return HOTP.generate(key, nbr)

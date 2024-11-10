5447494da3df2be1f59f553a284152b347a76883
Issam Abkadri <iabkadri@student.1337.ma>
2024-11-10 18:16:12 +0100

    fix: generate otp value

--- hotp.py
+++ hotp.py
@@ -9,18 +9,31 @@ class HOTP:
     """
 
     @classmethod
-    def generate(cls, secret: bytes, counter: int) -> int:
+    def generate(cls, secret: str, counter: int) -> str:
         """
         Return the number whose binary representation is s.
         """
-        hasher = hmac.new(secret, cls.int_to_bytes(counter),
-                            digestmod=sha1).digest()
+        msg = cls.int_to_bytes(counter)
+        key = cls.byte_secret(secret)
+        hasher = hmac.digest(key, msg, sha1)
         offset = hasher[-1] & 0xf
-        binary = ((hasher[offset] & 0x7f) << 24
-                    | (hasher[offset + 1] & 0xff) << 16
-                    | (hasher[offset + 2] & 0xff) << 8
-                    | (hasher[offset + 3]))
-        return binary % (10 ** 6)
+        binary = ((hasher[offset] & 0x7f) << 24 |
+                (hasher[offset + 1] & 0xff) << 16 |
+                (hasher[offset + 2] & 0xff) << 8 |
+                (hasher[offset + 3]) & 0xff)
+
+        result = str(binary % (10 ** 6))
+        result = result.rjust(6, '0')
+        return result
+
+    @staticmethod
+    def byte_secret(secret: str) -> bytes:
+        """
+        convert secret to bytes, right padding with zeros if necessary.
+        """
+        missing_padding = len(secret) % 8
+        secret = secret.rjust(missing_padding, '=')
+        return base64.b32decode(secret, casefold=True)
 
     @staticmethod
     def int_to_bytes(value: int) -> bytes:
@@ -31,4 +44,11 @@ class HOTP:
         while value:
             text.append(value & 0xff)
             value >>= 8
-        return bytes(reversed(text))
+        return bytes(bytearray(reversed(text)).rjust(8, b'\0'))
+
+    @staticmethod
+    def hex_str_to_bytes(hex_value: str) -> bytes:
+        """
+        convert hex value to a bytes value.
+        """
+        return bytes.fromhex(hex_value)
